e0c0a1514529749d99d0344e58ed5b6c4e9b4a1e
Sergio Vasquez <sergiovasquez122@gmail.com>
2019-08-29 08:54:46 -0700

    Partial completion of ItemGenerator class

--- src/ItemGenerator.java
+++ src/ItemGenerator.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * ItemGenerator class - Representation of single ItemGenerator
+ * @author Sergio Vasquez
+ */
 public class ItemGenerator {
+    /** Contains all possible items to be generated */
+    private ArrayList<Item> itemList;
+    /**
+     *  Constructor - fills the ItemGenerator with possible items
+     */
+    public ItemGenerator(){
+    }
+    /**
+     * Retrieves a random item from the ItemGenerator
+     * @return a random item from the ItemGenerator
+     */
+    public Item generateItems(){
+        Random generator = new Random();
+        int randomIndex = generator.nextInt(itemList.size());
+        Item i = itemList.get(randomIndex);
+        return new Item(i.getName());
+    }
 }
