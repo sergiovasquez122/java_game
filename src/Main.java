b5b18d15fbd7adad79cb68dd8a0a5018763ec5ee
Sergio Vasquez <sergiovasquez122@gmail.com>
2019-08-31 20:52:00 -0700

    Completion of enemyRoom method

--- src/Main.java
+++ src/Main.java
@@ -1,83 +1,91 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner userInput=  new Scanner(System.in);
-        System.out.println("What is your name?");
-        String name = userInput.nextLine();
         Map map = new Map();
+        Hero hero = new Hero("Luke",map);
         ItemGenerator itemGenerator = new ItemGenerator();
-        EnemyGenerator enemyGenerator = new EnemyGenerator(itemGenerator);
-        Hero hero = new Hero(name, map);
-        int currentMap = 1;
-        boolean gameOver = false;
-        while(!gameOver) {
-            hero.display();
-            hero.displayItems();
-            map.displayMap(hero.getLocation());
-
-            System.out.println("1. Go North");
-            System.out.println("2. Go South");
-            System.out.println("3. Go East");
-            System.out.println("4. Go West");
-            System.out.println("5. Quit");
-            int choice = userInput.nextInt();
-            if(choice != 5) {
-                char c = 'n';
-                switch (choice) {
-                    case 1: c = hero.goNorth();
-                            break;
-                    case 2: c = hero.goSouth();
-                            break;
-                    case 3: c = hero.goEast();
-                            break;
-                    case 4: c = hero.goWest();
-                            break;
-                }
-
-                if(c == 'e'){
-                    if(!enemyRoom(hero, map, enemyGenerator)){
-                        gameOver = true;
-                    }
-                } else if(c == 'i'){
-                    itemRoom(hero, map, itemGenerator);
-                } else if(c == 'f'){
-                    if(hero.hasKey()){
-                        hero.increaseLevel();
-                        hero.increaseMaxHP(10);
-                        currentMap++;
-                        map.loadMap(currentMap);
-                    }
-                }
-            } else{
-                gameOver = true;
-            }
+        for( int i = 0; i < 5; ++i){
+            hero.pickUpItem(itemGenerator.generateItem());
         }
-        System.out.println("Game Over");
+        itemRoom(hero,map,itemGenerator);
+        hero.displayItems();
     }
 
+    /**
+     *
+     * @param hero
+     * @param map
+     * @param enemyGenerator
+     * @return true if the hero is still alive
+     */
     public static boolean enemyRoom(Hero hero, Map map, EnemyGenerator enemyGenerator){
-        Enemy e = enemyGenerator.generateEnemy(hero.getLevel());
-        return fight(hero, e);
+        Enemy enemy = enemyGenerator.generateEnemy(hero.getLevel());
+        System.out.println("You've encountered a " + enemy.getName());
+        enemy.display();
+
+        String menu = "1. Fight\n2. Run Away\n";
+        boolean hero_died = false;
+        int num_of_options = 2;
+        // Hero has a med kit give them option of using it
+        if ( hero.hasMedKit() ){
+            menu += "3. Med Kit\n";
+            num_of_options ++;
+        }
+
+        int choice = CheckInput.getIntRange(1, num_of_options);
+        while(hero_died) {
+            if (choice == 1) {
+                hero_died = fight(hero, enemy);
+            } else if (choice == 2) {
+                // Run away to an adjacent cell
+                // where the cells are encodes as
+                // 0 = north, 1 = south, 2 = west, 3 = east
+                final int WALK_CHOICES = 4;
+                int random_walk = (int) Math.random() * WALK_CHOICES;
+                if(random_walk == 0){
+                    hero.goNorth();
+                } else if(random_walk == 1){
+                    hero.goSouth();
+                } else if(random_walk == 2){
+                    hero.goWest();
+                } else {
+                    hero.goEast();
+                }
+                return true;
+            } else {
+                hero.heal(25);
+                hero.removeItem("Med Kit");
+            }
+        }
+        return !hero_died;
     }
 
     public static boolean fight(Hero hero, Enemy e){
         return true;
     }
 
+    /**
+     *
+     * @param hero
+     * @param map
+     * @param itemGenerator
+     */
     public static void itemRoom(Hero hero, Map map, ItemGenerator itemGenerator){
         Item item = itemGenerator.generateItem();
-        // Does the hero have enough room in his inventory to pick up the item
-        if(hero.pickUpItem(item)){
+        System.out.println("You found a " + item.getName());
+        if( hero.pickUpItem(item) ){
             map.removeCharAtLoc(hero.getLocation());
-        } else{
-            System.out.println("Inventory full! Would you like to drop an item (y / n) ");
-            Scanner input = new Scanner(System.in);
-            String choice = input.nextLine();
-            if(choice.equals("y")){
-                System.out.println("Which item would you like to drop?");
-                int index = Integer.parseInt(input.nextLine());
-                hero.removeItem(index);
+        } else {
+            System.out.println("Inventory full would you like to drop an item?");
+            // User decided to drop an item
+            if( CheckInput.getYesNo() ){
+                System.out.println("What item would you liked to drop?");
+                hero.displayItems();
+
+                int index = CheckInput.getIntRange(1, 5);
+                hero.removeItem(index - 1);
                 hero.pickUpItem(item);
             }
         }
