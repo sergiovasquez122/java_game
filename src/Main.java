45a105e151b52dfb7d33243f4f1b04caab36cc3a
Sergio Vasquez <sergiovasquez122@gmail.com>
2019-09-08 14:35:50 -0700

    Refactored Main

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
         boolean gameOver = false;
 
 
-        while (!gameOver) {
+        while ( !gameOver ) {
 
 
             hero.display();
@@ -25,7 +25,7 @@ public class Main {
             int choice = CheckInput.getIntRange(1, 5);
 
             char c;
-            switch (choice) {
+            switch ( choice ) {
                 case 1:
                     c = hero.goNorth();
                     break;
@@ -43,7 +43,7 @@ public class Main {
                     return;
             }
 
-            switch (c) {
+            switch ( c ) {
                 case 'e':
                     gameOver = !enemyRoom(hero, map, enemyGenerator);
                     break;
@@ -64,7 +64,6 @@ public class Main {
 
     /**
      * Hero enters finishRoom, decides if hero moves to the next level or not
-     *
      * @param hero   the Hero of the game
      * @param map    the current map
      * @param mapNum the new map location to be loaded
@@ -73,12 +72,12 @@ public class Main {
     public static boolean finishRoom(Hero hero, Map map, int mapNum) {
 
         boolean moveOntoNextLevel = false;
-        if (hero.hasKey()) {
+        if ( hero.hasKey() ) {
             moveOntoNextLevel = true;
             hero.removeItem("Key");
         } else if (hero.hasHolocron() && !hero.hasKey()) {
             System.out.println(" Would you like to use the force to try to open the door?");
-            if (CheckInput.getYesNo()) {
+            if ( CheckInput.getYesNo() ) {
                 hero.removeItem("Holocron");
 
                 final int BOUND = 10;
@@ -90,9 +89,9 @@ public class Main {
             }
         }
 
-        if (moveOntoNextLevel) {
+        if ( moveOntoNextLevel ) {
             hero.increaseLevel();
-            map.loadMap(mapNum);
+            map.loadMap( mapNum );
             System.out.println("Now on level " + mapNum);
             return true;
         }
@@ -101,47 +100,46 @@ public class Main {
 
     /**
      * Method handles when Hero enters enemy room
-     *
      * @param hero           the current hero
      * @param map            the current map
      * @param enemyGenerator generates a random enemy
      * @return true if the hero is still alive false otherwise
      */
     public static boolean enemyRoom(Hero hero, Map map, EnemyGenerator enemyGenerator) {
-        Enemy enemy = enemyGenerator.generateEnemy(hero.getLevel());
-        System.out.println("You've encountered a " + enemy.getName());
+        Enemy enemy = enemyGenerator.generateEnemy( hero.getLevel() );
+        System.out.println( "You've encountered a " + enemy.getName() );
         enemy.display();
 
-        while (hero.getHP() != 0 && enemy.getHP() != 0) {
+        while ( hero.getHP() != 0 && enemy.getHP() != 0 ) {
             String menu = "1. Fight\n2. Run Away";
             int numOfOptions = 2;
             // Hero has a med kit give them option of using it
-            if (hero.hasMedKit()) {
+            if ( hero.hasMedKit() ) {
                 menu += "\n3. Med Kit";
                 numOfOptions++;
             }
 
-            System.out.println(menu);
-            int choice = CheckInput.getIntRange(1, numOfOptions);
-            switch (choice) {
+            System.out.println( menu );
+            int choice = CheckInput.getIntRange( 1, numOfOptions );
+            switch ( choice ) {
                 case 1:
-                    fight(hero, enemy);
-                    if (enemy.getHP() == 0) {
-                        map.removeCharAtLoc(hero.getLocation());
+                    fight( hero, enemy );
+                    if ( enemy.getHP() == 0 ) {
+                        map.removeCharAtLoc( hero.getLocation() );
                     }
                     break;
                 case 2:
-                    runAway(hero);
+                    runAway( hero );
                     return true;
                 case 3:
                     final int HEAL_AMOUNT = 25;
-                    hero.heal(HEAL_AMOUNT);
-                    hero.removeItem("Med Kit");
-                    if (hero.hasArmor()) {
+                    hero.heal( HEAL_AMOUNT );
+                    hero.removeItem( "Med Kit" );
+                    if ( hero.hasArmor() ) {
                         String armorName = hero.removeFirstArmorItem();
-                        System.out.println(hero.getName() + " defended himself with " + armorName);
+                        System.out.println( hero.getName() + " defended himself with " + armorName );
                     } else {
-                        enemy.attack(hero);
+                        enemy.attack( hero );
                     }
                     break;
             }
@@ -157,11 +155,11 @@ public class Main {
     public static void runAway(Hero hero) {
         Random random = new Random();
         Point oldLocation = hero.getLocation();
-        while (oldLocation.equals(hero.getLocation())) {
+        while ( oldLocation.equals( hero.getLocation() ) ) {
             final int BOUND = 4;
-            int walkDirection = random.nextInt(BOUND);
+            int walkDirection = random.nextInt( BOUND );
 
-            switch (walkDirection) {
+            switch ( walkDirection ) {
                 case 0:
                     hero.goNorth();
                     break;
@@ -187,28 +185,28 @@ public class Main {
      */
     public static boolean fight(Hero hero, Enemy e) {
 
-        hero.attack(e);
-        if (e.getHP() != 0) {
-            if (hero.hasArmor()) {
+        hero.attack( e );
+        if ( e.getHP() != 0 ) {
+            if ( hero.hasArmor() ) {
                 String armorName = hero.removeFirstArmorItem();
-                System.out.println(hero.getName() + " defended himself with " + armorName);
+                System.out.println( hero.getName() + " defended himself with " + armorName );
             } else {
-                e.attack(hero);
+                e.attack( hero );
             }
         } else {
-            System.out.println("You defeated the " + e.getName() + "!");
+            System.out.println( "You defeated the " + e.getName() + "!" );
             Item item = e.getItem();
-            if (hero.pickUpItem(item)) {
-                System.out.println("You received a " + item.getName() + " from the enemy.");
+            if ( hero.pickUpItem( item ) ) {
+                System.out.println(" You received a " + item.getName() + " from the enemy. ");
             } else {
-                System.out.println("Inventory full would you like to drop an item?");
+                System.out.println(" Inventory full would you like to drop an item? ");
                 // User decided to drop an item
-                if (CheckInput.getYesNo()) {
-                    System.out.println("What item would you liked to drop?");
+                if ( CheckInput.getYesNo() ) {
+                    System.out.println( "What item would you liked to drop?" );
                     hero.displayItems();
 
-                    int index = CheckInput.getIntRange(1, 5);
-                    hero.removeItem(index - 1);
+                    int index = CheckInput.getIntRange( 1, 5 );
+                    hero.removeItem( index - 1 );
                     hero.pickUpItem(item);
                 }
             }
@@ -226,20 +224,20 @@ public class Main {
      */
     public static void itemRoom(Hero hero, Map map, ItemGenerator itemGenerator) {
         Item item = itemGenerator.generateItem();
-        System.out.println("You found a " + item.getName());
-        if (hero.pickUpItem(item)) {
-            map.removeCharAtLoc(hero.getLocation());
+        System.out.println( "You found a " + item.getName() );
+        if ( hero.pickUpItem(item) ) {
+            map.removeCharAtLoc( hero.getLocation() );
         } else {
-            System.out.println("Inventory full would you like to drop an item?");
+            System.out.println( "Inventory full would you like to drop an item?" );
             // User decided to drop an item
-            if (CheckInput.getYesNo()) {
+            if ( CheckInput.getYesNo() ) {
                 System.out.println("What item would you liked to drop?");
                 hero.displayItems();
 
-                int index = CheckInput.getIntRange(1, 5);
-                hero.removeItem(index - 1);
-                hero.pickUpItem(item);
-                map.removeCharAtLoc(hero.getLocation());
+                int index = CheckInput.getIntRange( 1,  5);
+                hero.removeItem( index - 1);
+                hero.pickUpItem( item );
+                map.removeCharAtLoc( hero.getLocation() );
             }
         }
     }
